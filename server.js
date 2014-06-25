de44cb451d8ff751b7fc436838413262a709e42d
stew <stew@futurete.ch>
2014-06-25 20:18:12 +0400

    Added initial remote console html file. Setup some basic io routes on server.

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 var config = require('./config'),
-    app = require('express.io')();
+    express = require('express.io'),
+    app = express();
 
 app.http().io();
 
 app.listen(config.port);
+
+app.use("/", express.static(__dirname + '/public/'));
+
+app.io.route('target-connect', function(req) {
+  req.io.join(req.data);
+  req.io.broadcast('target connected');
+});
+
+app.io.route('console-connect', function(req) {
+  req.io.join(req.data);
+  req.io.broadcast('console connected');
+});
+
+app.io.route('command', function(req) {
+  req.io.broadcast('command ' + req.data);
+});
+
+app.io.route('log', function(req) {
+  req.io.broadcast('log ' + req.data);
+});
